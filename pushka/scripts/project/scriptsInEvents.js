9d499a9e567bc6fd8e63e56c282142b85c5448dc
pushkashot <103932980+pushkashot@users.noreply.github.com>
2023-09-27 16:52:33 +0300

    Add files via upload

---
+++ pushka/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async Game_Event7_Act29(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#1ec8fe'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
